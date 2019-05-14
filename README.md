901f684103400ff3c695b6745c39d64f35e6f069
Ruby Allison Rose <jtimmerman32@gmail.com>
2019-05-14 03:16:41 -0700

    Added multi-browser feature to notebook script.
    Also updated the readme with a tiny notice.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# notebook
+# M3TIOR's Jupyter Notebook
 My personal Jupyter Notebook made public for other peeps to grab at.
+
+I just found out that Github supports compilation of
+Jupyter Notebooks. So just navigate to which ever notebook *page* you think
+might be interesting and take a look. Not all are guaranteed to work, but most
+will. Especially ones that uses kernels as opposed to nonstandard magics,
+and native Jupyter features.
+
+This may also serve as a redundant storage location for some of my blog
+posts as I do plan on using my notebook for writing certain blog posts.
+If my blog is down for some reason, or you just want to look at my scraps
+as well you're welcome to it all.
+
+**Have at it!**
+
+Much love,<br/>
+M3TIOR
--- personal-notebook.py
+++ personal-notebook.py
@@ -17,32 +17,95 @@
 if __name__ != "__main__":
 	exit("This is supposed to be an executable script numbnuts, don't try and import it please.")
 
+
+# Metadata
+__author__="Ruby Allison Rose (aka: M3TIOR)"
+__version__="Go Fuck Yourself"
+#__all__=[ ] # Bite Me
+
+
 # Standard Library Imports
 import re, sys, os
 import signal
 from io import StringIO
 from pathlib import Path
 from threading import Event
+from argparse import ArgumentParser
 
+# External Library Imports
 try:
-	from sh import jupyter
+	from sh import which, jupyter_notebook as jupyter
 except ImportError:
 	print("""
 		Wow m8, is this me using this? Or someone else? You should probably
 		go ahead and run "pip install -r requirements.txt"... Try not to
 		make yourself look like an idiot okay? Sleep if you're tired.
 		If you're someone else you can disregard this. I have a bad habit of
-		overdoing shit.
+		overdoing shitself.
+
+		Ahem: ...make sure pip's sh & a Jupyter Notebook is installed!
+	""")
+	sys.exit(1)
+
+
+# Global Variables
+default_browser = "chromium-browser"
+supported_browsers = {
+	# Format: aliases : (command, [args, ...])
+	**dict.fromkeys(["chromium-browser", "chromium"], (
+		"chromium_browser", [
+			# loads a new chromium instance so this doesn't open in a pre-existing
+			# process and terminate early.
+			"--temp-profile",
+			# forces dark mode borders and such. Just in case they appear somehow.
+			"--force-dark-mode",
+			# removes the window borders and url bar.
+			"--app={notebook_url}"
+		]
+	)),
+	**dict.fromkeys(["surf"], (
+		"surf", [
+			"-BdfImnP", "{notebook_url}"
+		]
+	)),
+}
+
+
+# Main Application Start
+parser = ArgumentParser(description="M3TIOR's personal 'scientific' notebook.")
+# parser.add_argument('-o','--file',
+# 	help="Open's target ipython notebook file.",
+# 	action="store", type=str,
+# )
+parser.add_argument('-b', '--browser',
+	help="""
+		Selects the browser to use for opening the notebook.
+		Fails if that browser is unavailable.
+	""",
+	choices=supported_browsers,
+	default=default_browser
+)
+
+arguments = parser.parse_args(sys.argv[1:]);
+browser_name, browser_args = supported_browsers[arguments.browser]
+
+if arguments.file and not arguments.file.endswith(".ipynb"):
+	print("""
+		That's a problem, make sure you specify an ipython notebook file
+		instead of an arbitrairy file for the file open parameter, otherwise
+		Jupyter Notebook may get a little upsetty spaghetti.
+
+		Ahem: ...You passed the wrong file type m8.
 	""")
 	sys.exit(1)
 
 try:
-	from sh import chromium_browser
-except ImportError:
-	print("Error: you need chromium-browser to run this program.")
-	#print("Error: you need surf from suckless tools to run this program.")
-	sys.exit(1)
+	shb = __import__("sh", globals(), locals(), [browser_name], 0)
+	browser = getattr(shb, browser_name)
 
+except ImportError as e:
+	print("Error: could not find selected browser, maybe try a different one?")
+	exit(1)
 
 surfer = None
 joy = None
@@ -85,24 +148,17 @@ signal.signal(signal.SIGTERM, exit_handler)
 signal.signal(signal.SIGINT, exit_handler)
 
 # Runs a private notebook server in the directory this script is in.
-joy = jupyter.notebook(
+joy = jupyter(
 	"--no-browser",
 	"--notebook-dir", str(script.parent),
-
 	_bg=True, _out=joy_con, _err=joy_con
 )
 joy_started.wait() # wait for initialization to finish
 
 # Start the browser session.
-surfer = chromium_browser(
-	# loads a new chromium instance so this doesn't open in a pre-existing
-	# process and terminate early.
-	"--temp-profile",
-	# forces dark mode borders and such. Just in case they appear somehow.
-	"--force-dark-mode",
-	# removes the window borders and url bar.
-	"--app="+joy_key
-)
+surfer = browser(*(
+	arg.format(notebook_url=joy_key) for arg in browser_args
+))
 
 # When the browser is closed, close down the server
-jupyter.notebook.stop(joy_port)
+jupyter.stop(joy_port)
